5b398813721cade2c0c16ceb8ed879c3411eddad
Ivan Duncan <161537745+ImpactfulInfo@users.noreply.github.com>
2024-05-28 16:47:26 -0400

    Update HouseHold Data.sql

--- HouseHold Data.sql
+++ HouseHold Data.sql
@@ -1,7 +1,7 @@
 # CLEANING DATA
 
 Select * 
-from us_household_income
+FROM us_household_income
 ;
 SELECT * 
 FROM housing.us_household_income_statistics;
@@ -14,7 +14,7 @@ ALTER TABLE housing.us_household_income_statistics RENAME COLUMN `ï»¿id` TO `
 
 
 Select COUNT(id)
-from us_household_income
+FROM us_household_income
 ;
 SELECT COUNT(id)
 FROM housing.us_household_income_statistics;
@@ -27,115 +27,115 @@ FROM housing.us_household_income_statistics;
 
 
 Select * 
-from us_household_income
+FROM us_household_income
 ;
 SELECT * 
 FROM housing.us_household_income_statistics
 ;
 
 
-Select id, COUNT(id)
-from us_household_income
-group by id
-having COUNT(id) >1
+SELECT id, COUNT(id)
+FROM us_household_income
+GROUP BY id
+HAVING COUNT(id) >1
 
 
 ;
 SELECT *
 FROM (
-Select Row_id,
+SELECT Row_id,
 id,
 row_number() over (partition by id order by id) row_num
-from us_household_income
+FROM us_household_income
 ) duplicates
-where row_num >1
+WHERE row_num >1
 ;
 
-delete from us_household_income
-where ROW_ID in 
+DELETE FROM us_household_income
+WHERE ROW_ID in 
 ( SELECT ROW_ID
 FROM (
-Select Row_id,
+SELECT Row_id,
 id,
 row_number() over (partition by id order by id) row_num
-from us_household_income
+FROM us_household_income
 ) duplicates
-where row_num >1
+WHERE row_num >1
 )
 ;
-Select State_ab, count(distinct state_ab)
-from us_household_income
-group by state_ab
+SELECT State_ab, count(distinct state_ab)
+FROM us_household_income
+GROUP BY state_ab
 ;
 
-update us_household_income
-set state_name = 'Georgia'
-where state_name = 'georia'
+UPDATE us_household_income
+SET state_name = 'Georgia'
+WHERE state_name = 'georia'
 ;
-update us_household_income
-set state_name = 'Alabama'
-where state_name = 'alabama'
+UPDATE us_household_income
+SET state_name = 'Alabama'
+WHERE state_name = 'alabama'
 ;
-update us_household_income
-set place = 'Autaugaville'
-where county = 'Autauga County'
-and city = 'Vinemont'
+UPDATE us_household_income
+SET place = 'Autaugaville'
+WHERE county = 'Autauga County'
+AND city = 'Vinemont'
 ;
 Select type, COUNT(type)
 from us_household_income
 group by type
 ;
 
-update us_household_income
-set type='Borough'
-where type='Boroughs'
+UPDATE us_household_income
+SET type='Borough'
+WHERE type='Boroughs'
 ;
 
 
-select Aland, Awater
-from us_household_income
-where (Awater = 0 or Awater = '' or Awater = null)
-and (Aland = 0 or Aland = '' or Aland = null)
+SELECT Aland, Awater
+FROM us_household_income
+WHERE (Awater = 0 or Awater = '' or Awater = null)
+AND (Aland = 0 or Aland = '' or Aland = null)
 ;
 
 
 
 #EXPLORING DATA 
 
-select *
-from us_household_income
+SELECT *
+FROM us_household_income
 ;
 
-select *
-from us_household_income_statistics
+SELECT *
+FROM us_household_income_statistics
 ;
-select state_name, sum(Aland), SUM(Awater)
-from us_household_income
-group by state_name
-order by 1 asc
+SELECT state_name, sum(Aland), SUM(Awater)
+FROM us_household_income
+GROUP BY state_name
+ORDER BY 1 asc
 ;
 
 
-select i.state_name,  ROUND(AVG(Mean),2), ROUND(AVG(Median),2)
-from us_household_income i
- inner join us_household_income_statistics s
+SELECT i.state_name,  ROUND(AVG(Mean),2), ROUND(AVG(Median),2)
+FROM us_household_income i
+ INNER JOIN us_household_income_statistics s
 on i.id=s.id
-where mean <> 0
-group by i.State_Name
-order by 2 asc
+WHERE mean <> 0
+GROUP BY i.State_Name
+ORDER BY 2 asc
 ;
-select type,COUNT(Type),  ROUND(AVG(Mean),2), ROUND(AVG(Median),2)
-from us_household_income i
- inner join us_household_income_statistics s
+SELECT type,COUNT(Type),  ROUND(AVG(Mean),2), ROUND(AVG(Median),2)
+FROM us_household_income i
+ INNER JOIN us_household_income_statistics s
 on i.id=s.id
-where mean <> 0
-group by type
-having COUNT(Type) >100
-order by 2 asc
+WHERE mean <> 0
+GROUP BY type
+HAVING COUNT(Type) >100
+ORDER BY 2 asc
 ;
-select i.state_name, city, ROUND(AVG(MEAN),1),ROUND(AVG(MEDIAN),1)
-from us_household_income i
- inner join us_household_income_statistics s
+SELECT i.state_name, city, ROUND(AVG(MEAN),1),ROUND(AVG(MEDIAN),1)
+FROM us_household_income i
+INNER JOIN us_household_income_statistics s
 on i.id=s.id
-group by i.State_Name, city
-order by ROUND(AVG(MEAN),1) desc
+ GROUP BY i.State_Name, city
+ORDER BY ROUND(AVG(MEAN),1) desc
